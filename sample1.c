93d1f021f86d6bd9383738ffe981924e8497e2a4
al19034 <al19034@shibaura-it.ac.jp>
2022-07-12 04:03:09 +0900

    ”test”

---
+++ sample1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+int a,b;
+
+printf("1つ目の整数: a = ");
+scanf("%d",&a);
+
+printf("2つ目の整数: b = ");
+scanf("%d",&b);
+
+printf("a + b =%d\n",a+b);
+
+printf("a - b =%d\n",a-b);
+
+return 0;
+}
+
+
